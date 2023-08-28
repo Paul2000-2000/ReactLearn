4538696800f93ae796d0e6d2d14a7b0bb48cccd2
Paul2000-2000 <alexapaul2000@yahoo.com>
2023-08-28 16:03:44 +0300

    learning React basics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* const Greetingv2 = () => {
+
+    return ( 
+           <>
+            <div className=''>
+      <Person></Person>
+      <Da></Da>
+      <h1>merge</h1>
+      <img src='' alt=''/>
+      <input type='text' name='da' id='inp1' placeholder='mergeee'/>
+            </div>
+
+        <h3>
+          alt div blana 
+        </h3>
+            </> 
+    );
+ }
+ 
+ const Person = () => {
+  return (<div> <h4>This is john and this is my first component</h4> 
+                <h2>oare</h2>
+                </div> );}
+
+  const Da = () => {
+     return (
+        <p>Da ma da</p>
+     );
+  }
+
+/*
+ function Greeting() {
+       return  <h4>This is john and this is my first component</h4>;    
+}
+
+const Greetingv3 = () => {
+  return   React.createElement('h1' , {} , 'hello world')
+}
+*/
+
+
+function BookList() {
+   return (
+         <section>
+            <Book></Book>
+            <Book></Book>
+            <Book></Book>
+            <Book></Book>
+            <Book></Book>
+            <Book></Book>
+            <Book></Book>
+         </section>
+   );
+}
+
+const Book = () => {
+   return <article>
+      <Image></Image>
+      <Title></Title>
+      <Author></Author>
+   </article>
+};
+
+const Image = () => <img src='https://m.media-amazon.com/images/I/818K2QDCZvL._AC_UY218_.jpg' alt=''></img>
+
+const Title = () => <h1> This is a title</h1>
+
+const Author = () => <h2> This is an author</h2>
+
+ReactDOM.render(<BookList/>,document.getElementById('root'));
+
+//21 next
